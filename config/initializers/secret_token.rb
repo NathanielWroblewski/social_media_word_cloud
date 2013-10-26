027c90343cee9f7ff8a23c5655df0e32180ded91
Nathaniel Wroblewski <nathanielwroblewski@gmail.com>
2013-10-26 16:32:31 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SocialMediaWordCloud::Application.config.secret_key_base = '6ec93553606378204bccf5408cf90a9852039d56d03d120ce6a1893d4e3b453e90dee5aa6ff5f46967a90b000bc30c1fc215f828294f167dc92e2468ec8bdbbc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SocialMediaWordCloud::Application.config.session_store :cookie_store, key: '_social_media_word_cloud_session'
