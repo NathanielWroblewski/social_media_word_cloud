01c0496b31a353c38b3815bd2f1f9233e0a6e8aa
Nathaniel Wroblewski <NathanielWroblewski@gmail.com>
2013-12-01 13:30:58 -0800

    super sexy body parsing

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,14 @@
 class Post < ActiveRecord::Base
 
+  def self.words
+    self.all.flat_map(&:parse_body)
+  end
+
+  def parse_body
+    body.strip.downcase.gsub(/[^a-zA-Z]/, '').split(' ')
+  end
+
+  def self.word_frequencies
+    words.each_with_object(Hash.new(0)) { |word, freq| freq[word] += 1 }
+  end
 end
